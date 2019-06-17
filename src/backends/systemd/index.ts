9e08246f0f82a74a310a8c907f03be9572bbf009
vmarchaud <contact@vmarchaud.fr>
2019-06-17 21:45:18 +0200

    chore: remove old code

--- src/backends/systemd/index.ts
+++
@@ -1,8 +0,0 @@
-
-export * from './types/manager'
-export * from './types/job'
-export * from './types/service'
-export * from './types/unit'
-
-export * from './models/manager'
-export * from './models/unit'
--- src/backends/systemd/utils/bus.ts
+++
@@ -1,6 +0,0 @@
-
-import * as dbus from 'dbus-next'
-
-const systemBus = dbus.systemBus()
-
-export const getSystemBus = () => systemBus
